b7701781c8530f3c578df575606be5a9d83542a3
laura <lauragonsalves2000@gmail.com>
2022-10-07 23:50:46 +0600

    First commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const todayDate = require(__dirname + "/date.js")
+const todayDate = require(__dirname + "/date.js");
 
 const app = express();
 
